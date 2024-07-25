65c5f7ccf94ff14323418e24fcf7467755752ff2
Sourav Mazumdar <sourav.mozu@gmail.com>
2024-07-25 19:03:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,15 +5,17 @@ idx=0
 circles[idx].classList.add('active')
 function updatebuttons(){
 	if(idx==0)
-		prevbutton.disabled=true
-	else prevbutton.disabled=false
-	if(idx==4)
-		nextbutton.disabled=true
-	else nextbutton.disabled=false
+		{prevbutton.disabled=true}
+	else{ prevbutton.disabled=false}
+	
+	if(idx==circles.length-1)
+		{nextbutton.disabled=true}
+	else {nextbutton.disabled=false}
 }
 
 nextbutton.addEventListener(("click"),()=>{
-		if(idx<4){
+		if(idx<circles.length-1){
+			console.log("next")
 			idx++
 			circles[idx].classList.add('active')
 			updatebuttons()
@@ -21,6 +23,7 @@ nextbutton.addEventListener(("click"),()=>{
 })
 prevbutton.addEventListener(("click"),()=>{
 	if(idx>0){
+		console.log("prev")
 		circles[idx].classList.remove('active')
 		idx--
 		updatebuttons()
