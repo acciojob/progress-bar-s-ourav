bef2647cfcb7c6fbe4cd0a5dacab70bc0dae97cf
Sourav Mazumdar <sourav.mozu@gmail.com>
2024-07-25 18:33:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+circles=document.querySelectorAll(".circle")
+// console.log(circles);
+// console.log(circles[1])
+prevbutton=document.getElementById("prevbutton")
+nextbutton=document.getElementById("nextbutton")
+idx=0
+circles[idx].classList.add('active')
+function updatebuttons(){
+	if(idx==0)
+		prevbutton.disabled
+	if(idx==4)
+		nextbutton.disabled
+}
+
+nextbutton.addEventListener(("click"),()=>{
+		if(idx<4){
+			idx++
+			circles[idx].classList.add('active')
+			updatebuttons()
+		}
+})
+prevbutton.addEventListener(("click"),()=>{
+	if(idx>=0){
+		circles[idx].classList.remove('active')
+		idx--
+		updatebuttons()
+	}
+})
+
